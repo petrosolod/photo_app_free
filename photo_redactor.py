aa54c7e9a0771ea4781b255a316b67848618cb98
petrosolod <119485704+petrosolod@users.noreply.github.com>
2023-02-19 17:14:50 +0200

    _

--- photo_redactor.py
+++ photo_redactor.py
@@ -1,1 +1,9 @@
-print('test')
+import os
+from PyQt6.QtWidgets import QApplication, QWidget, QFileDialog, QLabel, QPushButton, QListWidget, QHBoxLayout, QVBoxLayout, QMessageBox
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap
+
+from PIL import Image 
+from PIL.ImageFilter import DETAIL, BLUR
+
+app = QApplication
