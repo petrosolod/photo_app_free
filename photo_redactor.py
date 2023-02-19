4d4bfdc5b6dd74a02bf572e768f91d7338e9c7b6
petrosolod <119485704+petrosolod@users.noreply.github.com>
2023-02-19 18:47:00 +0200

    god damn

--- photo_redactor.py
+++ photo_redactor.py
@@ -6,4 +6,61 @@ from PyQt6.QtGui import QPixmap
 from PIL import Image 
 from PIL.ImageFilter import DETAIL, BLUR
 
+
+class ImageProcessor:
+    def __init__(self): 
+        pass
+
+    def load_image(self, filename):
+        pass
+
+    def save_image(self): 
+        pass
+
+    def bl_wt(self): 
+        pass
+
+    def to_left(self): 
+        pass
+
+    def to_right(self):
+        pass
+
+    def flip(self): 
+        pass
+
+    def go_blur(self):
+        pass
+
+
+def filter_images(files, extentions):
+    result = []
+    for filename in files:
+        for ext in extentions:
+            if filename.endswith(ext):
+                result.append(filename)
+    return result 
+
+
+def choose_workdir():
+    global workdir 
+    workdir = QFileDialog.getExistingDirectory
+
+def show_name_list():
+    extentions = ['.jpg', '.jpeg', '.bmp', '.gif', '.png' ]
+    choose_workdir()
+    filenames = filter_images(os.listdir(workdir), extentions)
+
+    
+def show_info():
+    my_info = QMessageBox()
+    my_info.setText('Demo photo \nVer 1.0')
+
+
+
 app = QApplication
+window = QWidget()
+window.resize(1200, 850)
+window.setWindowTitle('Photo Redactor (v.0.0.1)')
+
+
